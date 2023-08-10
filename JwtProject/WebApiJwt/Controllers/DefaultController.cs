158a6580d6391e7c3514479f43a29a2931321141
Kadir-Aydemir <kadiraydemir323@gmail.com>
2023-08-10 17:06:29 +0300

    Test Json Web Token

---
+++ JwtProject/WebApiJwt/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiJwt.Models;
+
+namespace WebApiJwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult CreateToken()
+        {
+            return Ok(new CreateToken().TokenCreate());
+        }
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Test2()
+        {
+            return Ok("Hoşgeldiniz");
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult CreateTokenForAdmin()
+        {
+            return Ok(new CreateToken().TokenCreateForAdmin());
+        }
+
+        [Authorize(Roles ="Admin,Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult Test4()
+        {
+            return Ok("Token başarılı bir şekilde giriş yaptı.");
+        }
+    }
+}
